be7bf2a7e005dfcf7260938c17a7803e033e6365
sakshi thakur <122427915+sakshiThakur-7@users.noreply.github.com>
2023-04-28 20:59:24 +0530

    Day 2 Assignment Submitted

---
+++ Day2Assignment.py
@@ -0,0 +1,9 @@
+number=int(input("enter number to check whether no. is even or odd = "))
+def even(number):
+    if number%2 == 0:
+        print("entered number is",number," even")
+    else:
+        print("entered number is",number," odd")
+even(number)
+
+
